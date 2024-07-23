da21ebffedf782d2c56d0b5d9af16d439970d55f
Shifana2 <shifanabarira@gmail.com>
2024-07-23 16:48:36 +0530

    Add files via upload

---
+++ day13t1.js
@@ -0,0 +1,10 @@
+document.querySelector('.accept').addEventListener('click', function() {
+    alert('You clicked Accept!');
+});
+
+document.querySelector('.decline').addEventListener('click', function() {
+    alert('You clicked Decline!');
+});
+
+
+
